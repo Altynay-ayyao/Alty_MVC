fcae85cfb6e733df512ebf654a98cb9506fde362
Altynay <altyn@Altun_kotikone.(none)>
2024-01-02 17:45:41 +0200

    server side and client side validations added

--- AltyWeb/Controllers/CategoryController.cs
+++ AltyWeb/Controllers/CategoryController.cs
@@ -26,14 +26,23 @@ namespace AltyWeb.Controllers
 
         [HttpPost]
         public IActionResult Create(Category obj)
-        {
+        {   
+           
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                return RedirectToAction("Index");
             }
-           
-            return RedirectToAction("Index");
+            return View();
+            
         }
     }
 }
